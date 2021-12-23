69ae4eec553f2bdfa2406f2d37a962a8cc8f372a
Tyler Crouch <tyle93@github.com>
2021-12-22 20:33:48 -0500

    Swapped Credentials to avoid need for user input.

--- Program.cs
+++ Program.cs
@@ -5,21 +5,24 @@ using Google.Apis.Services;
 using Google.Apis.Util.Store;
 using Microsoft.Win32;
 using System.Management;
+using Newtonsoft.Json.Linq;
+using System.Text;
 using System.Diagnostics;
 
-string UTGRegPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Future P.O.S.\\DIRECTORIES\\";
-string FPOSRegPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Shift4 Corporation\\";
+string FPOSRegPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Future P.O.S.\\DIRECTORIES\\";
+string UTGRegPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Shift4 Corporation\\";
 string UTGRegValueName = "Installation Path";
 string FPOSRegVauleName = "FPOS Directory";
 string? UTGInstallPath;
 string? FPOSInstallPath;
 string InstanceName = "";
-string ServerName = "";
+string ServerName = Environment.MachineName;
 string FPOSVersion = "";
 string UTGVersion = "";
 int FPOSMajorVersion;
 
 try{
+    Console.WriteLine(UTGRegPath + "\t" + UTGRegValueName);
     UTGInstallPath = (string?)Registry.GetValue(UTGRegPath,UTGRegValueName, "Value not found");
     if(UTGInstallPath == null){
         UTGInstallPath = "Not Installed";
@@ -28,7 +31,7 @@ try{
     }else{
         UTGInstallPath += "\\UTG2\\UTG2.exe";
         try{
-            UTGVersion = FileVersionInfo.GetVersionInfo(UTGInstallPath).ToString();
+            UTGVersion = FileVersionInfo.GetVersionInfo(UTGInstallPath).FileVersion ?? "N/a";
             Console.WriteLine($"UTG VERSION: {UTGVersion}");
         }catch(FileNotFoundException e){
             Console.Error.WriteLine(e.Message);
@@ -52,7 +55,7 @@ try{
         try{
             FileVersionInfo FPOSVer = FileVersionInfo.GetVersionInfo(FPOSInstallPath);
             FPOSMajorVersion =  FPOSVer.FileMajorPart;
-            FPOSVersion = FPOSVer.ToString();
+            FPOSVersion = FPOSVer.FileVersion ?? "N/a";
             Console.WriteLine($"FPOS VERSION: {FPOSVersion}");
             switch(FPOSMajorVersion){
                 case 5:
@@ -86,25 +89,33 @@ string QueryString = @"
 ";
 
 string CurrentUser = Environment.UserName;
-string SqlcmdOutputFilePath = "Sqloutput.csv";
-string PSCommand = $" -S {ServerName}\\{InstanceName} -Q {QueryString} -o {SqlcmdOutputFilePath} ";
+string SqlcmdOutputFilePath = "./Sqloutput.csv";
+string PSCommand = $" -S {ServerName}\\{InstanceName} -Q {QueryString} -o {SqlcmdOutputFilePath} -W -h -1 -s \",\"";
 File.Create($".\\{SqlcmdOutputFilePath}").Close();
 
 try{
-    Process.Start("sqlcmd.exe",PSCommand);
+    Process.Start("sqlcmd.exe",PSCommand).WaitForExit();
 }catch(Exception e){
     Console.WriteLine(e.Message);
 }
 
-List<string> rd = new List<string>(); //File.ReadAllText(SqlcmdOutputFilePath).Split(',').ToList();
-rd.Add(FPOSVersion);
-rd.Add(UTGVersion);
+List<string> rd = new List<string>();
+rd.Add(Environment.MachineName);
+if(File.Exists(SqlcmdOutputFilePath)){
+    Console.WriteLine($"Path: {SqlcmdOutputFilePath} exists.");
+    rd.AddRange(File.ReadAllText(SqlcmdOutputFilePath,System.Text.Encoding.UTF8).Split(',').ToList());
+    rd.Add(FPOSVersion);
+    rd.Add(UTGVersion);
+}else{
+    Console.WriteLine($"Path: {SqlcmdOutputFilePath} does not exitst.");
+    rd = new List<string>();
+}
 
-string TotalSystemMemory = "0";
+string TotalSystemMemory = "N/a";
 
 try{
     ObjectQuery wql = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
-    ManagementObjectSearcher searcher = new ManagementObjectSearcher(wql);
+    ManagementObjectSearcher searcher =  new ManagementObjectSearcher(wql);
     ManagementObjectCollection results = searcher.Get();
     foreach(var s in results){
         string? bytes = s["TotalPhysicalMemory"].ToString();
@@ -116,26 +127,15 @@ try{
 
 rd.Add(TotalSystemMemory);
 
-string[] scopes = {SheetsService.Scope.Spreadsheets, SheetsService.Scope.Drive};
+string[] scopes = {SheetsService.Scope.Spreadsheets};
 string ApplicationName = "System Audit";
 
-UserCredential credential;
-ClientSecrets cs = new ();
+string credJsonString = @"{}";
 
-if(args.Length >= 2){
-    cs.ClientId = args[0];
-    cs.ClientSecret = args[1];
-}else{
-    cs.ClientId = "";
-    cs.ClientSecret = "";
-}
-
-string credPath = ".\\token.json";
-credential = GoogleWebAuthorizationBroker.AuthorizeAsync(cs,scopes,"user",CancellationToken.None,new FileDataStore(credPath, true)).Result;
-Console.WriteLine("Credential file saved to: " + credPath);
+var cred = GoogleCredential.FromStream(new MemoryStream(System.Text.Encoding.ASCII.GetBytes(credJsonString)));
 
 var service = new SheetsService(new BaseClientService.Initializer(){
-                HttpClientInitializer = credential,
+                HttpClientInitializer = cred,
                 ApplicationName = ApplicationName,
 });
 
@@ -164,7 +164,3 @@ batch.Requests = new List<Request>();
 batch.Requests.Add(req);
 
 service.Spreadsheets.BatchUpdate(batch,SpreadSheetID).Execute();
-
-
-
-
