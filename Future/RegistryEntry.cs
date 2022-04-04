f33eda7a5c10f0118dedc60288ac06b449102ae3
Tyler Crouch <tyle93@github.com>
2022-04-04 11:35:28 -0700

    Small Refactor

--- Future/RegistryEntry.cs
+++ Future/RegistryEntry.cs
@@ -1,34 +1,21 @@
-using Microsoft.Win32;
+
 using System.Diagnostics;
 namespace Future.Registry{
     public static class RegistryEntry{
-        public static readonly string FPOSRegPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Future P.O.S.\\DIRECTORIES\\";
-        public static readonly string UTGRegPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Shift4 Corporation\\";
-        public static readonly string UTGRegValueName = "Installation Path";
-        public static readonly string FPOSRegValueName = "FPOS Directory";
-        public static readonly string? UTGInstallPath;
-        public static readonly string? FPOSInstallPath;
-        public static readonly FileVersionInfo? FPOSVersionInfo;
-        public static readonly string FPOSVersion;
-        public static readonly string UTGVersion;
-        public static readonly int? FPOSVersionMajor;
-        public static readonly string InstanceName;
+        public static string FPOSRegPath{get; private set;}= "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Future P.O.S.\\DIRECTORIES\\";
+        public static string UTGRegPath{get; private set;} = "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Shift4 Corporation\\";
+        public static string UTGRegValueName{get; private set;} = "Installation Path";
+        public static string FPOSRegValueName{get; private set;}= "FPOS Directory";
+        public static string? UTGInstallPath{get; private set;}
+        public static string? FPOSInstallPath{get; private set;}
+        public static FileVersionInfo? FPOSVersionInfo{get; private set;}
+        public static string FPOSVersion{get; private set;}
+        public static string UTGVersion{get; private set;}
+        public static int? FPOSVersionMajor{get; private set;}
+        public static string InstanceName{get; private set;}
         static RegistryEntry(){
-
-            UTGInstallPath  = (string?)Microsoft.Win32.Registry.GetValue(UTGRegPath,UTGRegValueName, null);
-            try{
-                UTGVersion = FileVersionInfo.GetVersionInfo(UTGInstallPath ?? "").FileVersion ?? "N/a";
-            }catch(FileNotFoundException ex){
-                Console.Error.WriteLine(ex.Message);
-                UTGVersion = "N/a";
-            }
-            FPOSInstallPath = (string?)Microsoft.Win32.Registry.GetValue(FPOSRegPath,FPOSRegValueName, null);
-            try{
-                FPOSVersionInfo = FileVersionInfo.GetVersionInfo(FPOSInstallPath ?? "");
-            }catch(FileNotFoundException ex){
-                Console.Error.WriteLine(ex.Message);
-                FPOSVersionInfo = null;
-            }
+            InitUTG();
+            InitFPOS();
             FPOSVersion = FPOSVersionInfo?.FileVersion ?? "N/a";
             FPOSVersionMajor = FPOSVersionInfo?.FileMajorPart ?? null;
             InstanceName = FPOSVersionMajor switch {
@@ -37,10 +24,33 @@ namespace Future.Registry{
                 _ => " ",
             };
         }
+            
+        private static void InitUTG(){
+            UTGInstallPath  = (string?)Microsoft.Win32.Registry.GetValue(UTGRegPath,UTGRegValueName, null);
+            try{
+                UTGVersion = FileVersionInfo.GetVersionInfo(UTGInstallPath ?? "").FileVersion ?? "N/a";
+            }catch(FileNotFoundException ex){
+                Console.Error.WriteLine(ex.Message);
+                UTGVersion = "N/a";
+            }
+        }
+        private static void InitFPOS(){
+            FPOSInstallPath = (string?)Microsoft.Win32.Registry.GetValue(FPOSRegPath,FPOSRegValueName, null);
+            try{
+                FPOSVersionInfo = FileVersionInfo.GetVersionInfo(FPOSInstallPath ?? "");
+            }catch(FileNotFoundException ex){
+                Console.Error.WriteLine(ex.Message);
+                FPOSVersionInfo = null;
+            }
+        }
     }
     public static class Util{
         public static string? getServerName(){
             return $"{Environment.MachineName}\\{RegistryEntry.InstanceName}";
+        }
+
+        public static string getHostName(){
+            return $"{Environment.MachineName}";
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,12 +2,10 @@
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
 using Google.Apis.Services;
-using Microsoft.Win32;
 using System.Management;
-using Newtonsoft.Json.Linq;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
-using NickStrupat;
+
 
 if(!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)){
     Console.Error.WriteLine("Platform not supported.");
@@ -25,6 +23,7 @@ string QueryString = @"
                 ""
 ";
 
+string MachineName = Environment.MachineName;
 string CurrentUser = Environment.UserName;
 string SqlcmdOutputFilePath = "./Sqloutput.csv";
 string PSCommand = $" -S {Future.Registry.Util.getServerName}-Q {QueryString} -o {SqlcmdOutputFilePath} -W -h -1 -s \",\"";
@@ -37,37 +36,32 @@ try{
 }
 
 List<string> rd = new List<string>();
-rd.Add(Environment.MachineName);
+rd.Add("STORE NAME");
+rd.Add(Future.Registry.Util.getHostName());
 if(File.Exists(SqlcmdOutputFilePath)){
     rd.AddRange(File.ReadAllText(SqlcmdOutputFilePath,System.Text.Encoding.UTF8).Split(',').ToList());
     rd.Add(Future.Registry.RegistryEntry.FPOSVersion);
     rd.Add(Future.Registry.RegistryEntry.UTGVersion);
 }else{
     Console.WriteLine($"Path: {SqlcmdOutputFilePath} does not exitst.");
-    rd = new List<string>();
 }
 
-//Pray this works.
-string TotalSystemMemory = "N/a";
-ComputerInfo info = new ComputerInfo();
-TotalSystemMemory = Math.Round((double)(info.TotalPhysicalMemory / (1024*1024)),2).ToString() + "GB";
+string TotalSystemMemory = "0 GB";
 
+try{
+    ObjectQuery wql = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+    ManagementObjectSearcher searcher =  new ManagementObjectSearcher(wql);
+    ManagementObjectCollection results = searcher.Get();
+    foreach(var s in results){
+        double res = Convert.ToDouble(s["TotalVisibleMemorySize"]);
+        res = Math.Round((res / (1024*1024)),2);
+                Console.WriteLine("Total usable memory size: "+ TotalSystemMemory +"GB");
+        TotalSystemMemory = res.ToString() + "GB";
 
-//TODO: FIX THIS!
-//try{
-//    ObjectQuery wql = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
-//    ManagementObjectSearcher searcher =  new ManagementObjectSearcher(wql);
-//    ManagementObjectCollection results = searcher.Get();
-//    foreach(var s in results){
-//        double res = Convert.ToDouble(s["TotalVisibleMemorySize"]);
-//        res = Math.Round((res / (1024*1024)),2);
-//                Console.WriteLine("Total usable memory size: "+ TotalSystemMemory +"GB");
-//        TotalSystemMemory = res.ToString();
-//
-//    }
-//}catch(Exception e){
-//    Console.WriteLine(e.StackTrace);
-//}
+    }
+}catch(Exception e){
+    Console.WriteLine(e.StackTrace);
+}
 
 rd.Add(TotalSystemMemory);
 
